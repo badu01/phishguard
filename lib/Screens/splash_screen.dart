9f6fa3e16fb7412cd534bda2f1e681c89c92a92a
badu01 <badushapareeth777@gmail.com>
2024-02-28 17:05:51 +0530

    Initial commit-Splash screen added

---
+++ lib/Screens/splash_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ScreenSplash extends StatefulWidget {
+  const ScreenSplash({super.key});
+
+  @override
+  State<ScreenSplash> createState() => _ScreenSplashState();
+}
+
+class _ScreenSplashState extends State<ScreenSplash> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Slapsh"),
+      ),
+    );
+  }
+}
